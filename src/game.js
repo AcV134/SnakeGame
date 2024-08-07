3ed3f5c46b15a002880fb61e351e6c5afe91f0f9
AcV134 <anirudh.c.vinayan@gmail.com>
2024-08-07 22:18:55 +0530

    Fix sound

--- src/game.js
+++ src/game.js
@@ -158,7 +158,7 @@ function gameOver(){
 
 // on clicking button
 function buttonClick(){
-    let audio = new Audio("../sound/game-start.mp3");
+    let audio = new Audio("./sound/game-start.mp3");
     audio.play();
     let heading = $('.title');
     heading.css('animation','none');
